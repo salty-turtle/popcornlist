88410f018e9e7f6619ea880a29fd77e5e2ecc76f
justintd <justintdinh1@gmail.com>
2020-06-13 01:39:36 -0700

    Added NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./NavBar.scss";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="main-nav">
+        <div>Test</div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
