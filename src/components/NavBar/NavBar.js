c1eae94e13cc47a554558393bf1a3ad724bfa5e9
justintd <justintdinh1@gmail.com>
2020-06-13 15:55:57 -0700

    Added NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import "./NavBar.scss";
+import logo from "../../images/popcorn.png";
 
 class NavBar extends Component {
   render() {
     return (
-      <nav className="main-nav">
-        <div>Test</div>
-      </nav>
+      <div className="nav-container">
+        <div className="nav-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav className="nav-items">
+          <ul>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV Shows</a>
+            </li>
+            <li>
+              <a href="#">People</a>
+            </li>
+            <li>
+              <a href="#">SearchBar</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
