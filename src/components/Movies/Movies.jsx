dd441299ed978460e4092b4b92809ffb8d10d60f
justintd <justintdinh1@gmail.com>
2020-06-17 04:13:15 -0700

    Refactored components

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 function Movies() {
   return <div>Movies</div>;
--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 function People() {
   return <div>People</div>;
