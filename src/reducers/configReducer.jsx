9b4df6547c85ac85d3d935365c6ac69473b22c8c
justintd <justintdinh1@gmail.com>
2020-06-20 02:34:57 -0700

    Fixed configReducer

--- src/reducers/configReducer.jsx
+++ src/reducers/configReducer.jsx
@@ -3,7 +3,10 @@ import * as types from "../actions/types";
 export default (state = {}, action) => {
   switch (action.type) {
     case types.REQUEST_CONFIG:
-      return { base: action.payload };
+      return {
+        ...state,
+        base: action.payload,
+      };
     default:
       return state;
   }
