48ee7a32458499736e740d5b42a5ae494ecb2260
Gabriel Ng <kingwai715@gmail.com>
2020-06-23 18:08:43 -0400

    Set up rendering of movie posters

--- src/components/Carousel/MovieCard.jsx
+++ src/components/Carousel/MovieCard.jsx
@@ -5,8 +5,18 @@ import "./MovieCard.scss";
 
 function MovieCard(props) {
   return (
-    console.log(props.movie),
-    <div>movie</div>)
+    console.log(props.movie, props.config),
+    (
+      <div className="card-container">
+        <img
+          src={`${props.config.base.images.secure_base_url}${props.config.base.images.poster_sizes[1]}${props.movie.poster_path}`}
+        />
+        <div>
+          {props.movie.original_title}
+        </div>
+      </div>
+    )
+  );
 }
 
 export default MovieCard;
