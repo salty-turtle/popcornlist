1fc04c32169525077af2e079ad10896724bba2aa
justintd <justintdinh1@gmail.com>
2020-06-25 13:55:20 -0700

    Added movie details and styling to MovieDetails

--- src/components/HomeSlide/HomeSlide.jsx
+++ src/components/HomeSlide/HomeSlide.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { requestPopularMovies } from "../../redux/actions/index";
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,17 +1,57 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { requestMovie } from "../../redux/actions/index";
+import "./MovieDetails.scss";
+import { findByLabelText } from "@testing-library/react";
 
 function MovieDetails(props) {
   const { movieId } = useParams();
   const dispatch = useDispatch();
 
+  const config = useSelector((state) => state.config);
+  const movie = useSelector((state) => state.movie);
+  const moviePoster = `${config.base.images.secure_base_url}${config.base.images.poster_sizes[4]}${movie.poster_path}`;
+
   useEffect(() => {
     dispatch(requestMovie(movieId));
-  });
+  }, []);
 
-  return <div>{movieId}</div>;
+  return movie.loading ? (
+    <div>LOADING...</div>
+  ) : (
+    <div>
+      <div className="movie-wrapper">
+        <div className="movie-container">
+          <div
+            className="movie-poster"
+            style={{
+              background: `url(${config.base.images.secure_base_url}${config.base.images.poster_sizes[4]}${movie.poster_path}) no-repeat`,
+              backgroundSize: "cover",
+              borderRadius: "20px",
+              minWidth: "400px",
+              minHeight: "625px",
+              marginRight: "100px",
+            }}
+          ></div>
+          <div className="movie-text-container">
+            <div className="movie-title">{movie.original_title}</div>
+            <div className="movie-tagline">{movie.tagline}</div>
+            <div className="movie-info">
+              <i className="fas fa-star"></i> {movie.vote_average} Rating |
+              Science Fiction, Drama {/*CHANGE GENRE*/} | English{" "}
+              {/* CHANGE ORIGINAL LANGUAGE */} | {movie.runtime} min.
+            </div>
+            <div className="movie-extra-details">[ MORE DETAILS HERE ]</div>
+            <div className="movie-secondary-title">Synopsis</div>
+            <div className="movie-synopsis">{movie.overview}</div>
+            <div className="movie-secondary-title">Cast</div>
+            <div className="movie-cast"></div>
+            <div classname="buttons-container"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default MovieDetails;
