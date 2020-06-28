bda196c59019e817716aada046d85d5b78d09678
justintd <justintdinh1@gmail.com>
2020-06-28 03:27:37 -0700

    Added clickable link to MovieCard images

--- src/components/Carousel/MovieCard.jsx
+++ src/components/Carousel/MovieCard.jsx
@@ -1,26 +1,25 @@
 import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { requestPopularMovies } from "../../redux/actions/index";
+import { Link } from "react-router-dom";
 import "./Carousel.scss";
 
 function MovieCard(props) {
   return (
-    (
-      <div className="swiper-slide card-slide">
+    <div className="swiper-slide card-slide">
+      <Link to={`/movies/${props.movie.id}`}>
         <img
           className="card-img"
           src={`${props.config.images.secure_base_url}${props.config.images.poster_sizes[1]}${props.movie.poster_path}`}
         />
-        <div className="card-details-container">
-          <div className="card-details-title">
-            {`${props.movie.title} `}
-          </div>
-          <div className="card-details-genre">
-            {props.movie.genre_ids.map((id) => `${props.genreList.get(id)} `)}
-          </div>
+      </Link>
+      <div className="card-details-container">
+        <div className="card-details-title">{`${props.movie.title} `}</div>
+        <div className="card-details-genre">
+          {props.movie.genre_ids.map((id) => `${props.genreList.get(id)} `)}
         </div>
       </div>
-    )
+    </div>
   );
 }
 
