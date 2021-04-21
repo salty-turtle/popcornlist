a7c2cadd7b719f67f44fc29f0e3ef9556deb80b0
justintd <justintdinh1@gmail.com>
2021-04-21 15:15:53 -0700

    Adjusts mobile nav and toggle to more practical solution

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
-import "./NavBar.scss";
-import logo from "../../images/logo.svg";
-import { Link, useHistory } from "react-router-dom";
+import React, {useState} from 'react';
+import './NavBar.scss';
+import logo from '../../images/logo.svg';
+import {Link, useHistory} from 'react-router-dom';
 
 function NavBar() {
   let history = useHistory();
 
-  const [searchStr, setSearchStr] = useState("");
+  const [searchStr, setSearchStr] = useState('');
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
   function handleChange(e) {
     setSearchStr(e.target.value);
@@ -19,13 +24,10 @@ function NavBar() {
 
   return (
     <div>
-      <input type="checkbox" id="hamburger-toggle" />
       <div className="nav-container">
-        <div className="hamburger-toggle-container">
-          <label for="hamburger-toggle">
-            <i className="fas fa-bars hamburger-toggle-icon"></i>
-          </label>
-        </div>
+        <button className="hamburger-menu" type="button" onClick={handleToggle}>
+          {toggle ? <i class="fas fa-times" /> : <i class="fas fa-bars" />}
+        </button>
         <nav className="nav-items">
           <ul className="menu">
             <li>
@@ -50,12 +52,12 @@ function NavBar() {
           </ul>
         </nav>
         <div className="nav-search-container">
-          <form className="nav-search-form" onSubmit={(e) => handleSubmit(e)}>
+          <form className="nav-search-form" onSubmit={e => handleSubmit(e)}>
             <input
               type="text"
               className="nav-search-box"
               placeholder="Search Movies, TV Shows..."
-              onChange={(e) => handleChange(e)}
+              onChange={e => handleChange(e)}
             />
             <button type="submit" className="nav-search-btn">
               <i className="fas fa-search"></i>
@@ -63,7 +65,25 @@ function NavBar() {
           </form>
         </div>
       </div>
-      <div className="nav-padding"></div>
+      <div className="nav-padding" />
+      {toggle && (
+        <nav className="nav-items">
+          <ul className="menu-mobile">
+            <li className="nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/shows">TV Shows</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/people">People</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
