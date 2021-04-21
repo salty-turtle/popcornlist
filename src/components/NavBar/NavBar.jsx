7eecd62cf56954cbfc1e6e49d0547d36dbc59e38
justintd <justintdinh1@gmail.com>
2021-04-21 15:31:50 -0700

    Adds overlay and click handlers to toggle hamburger

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -23,68 +23,71 @@ function NavBar() {
   }
 
   return (
-    <div>
-      <div className="nav-container">
-        <button className="hamburger-menu" type="button" onClick={handleToggle}>
-          {toggle ? <i class="fas fa-times" /> : <i class="fas fa-bars" />}
-        </button>
-        <nav className="nav-items">
-          <ul className="menu">
-            <li>
-              <div className="nav-logo">
-                <Link to="/">
-                  <img src={logo} alt="logo" />
-                </Link>
-              </div>
-            </li>
-            <li className="nav-item">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/movies">Movies</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/shows">TV Shows</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/people">People</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="nav-search-container">
-          <form className="nav-search-form" onSubmit={e => handleSubmit(e)}>
-            <input
-              type="text"
-              className="nav-search-box"
-              placeholder="Search Movies, TV Shows..."
-              onChange={e => handleChange(e)}
-            />
-            <button type="submit" className="nav-search-btn">
-              <i className="fas fa-search"></i>
-            </button>
-          </form>
+    <>
+      <div>
+        <div className="nav-container">
+          <button className="hamburger-menu" type="button" onClick={handleToggle}>
+            {toggle ? <i class="fas fa-times" /> : <i class="fas fa-bars" />}
+          </button>
+          <nav className="nav-items">
+            <ul className="menu">
+              <li>
+                <div className="nav-logo">
+                  <Link to="/">
+                    <img src={logo} alt="logo" />
+                  </Link>
+                </div>
+              </li>
+              <li className="nav-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/shows">TV Shows</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/people">People</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="nav-search-container">
+            <form className="nav-search-form" onSubmit={e => handleSubmit(e)}>
+              <input
+                type="text"
+                className="nav-search-box"
+                placeholder="Search Movies, TV Shows..."
+                onChange={e => handleChange(e)}
+              />
+              <button type="submit" className="nav-search-btn">
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
+          </div>
         </div>
+        <div className="nav-padding" />
+        {toggle && (
+          <nav className="nav-items">
+            <ul className="menu-mobile">
+              <li className="nav-item" onClick={handleToggle}>
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item" onClick={handleToggle}>
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li className="nav-item" onClick={handleToggle}>
+                <Link to="/shows">TV Shows</Link>
+              </li>
+              <li className="nav-item" onClick={handleToggle}>
+                <Link to="/people">People</Link>
+              </li>
+            </ul>
+          </nav>
+        )}
       </div>
-      <div className="nav-padding" />
-      {toggle && (
-        <nav className="nav-items">
-          <ul className="menu-mobile">
-            <li className="nav-item">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/movies">Movies</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/shows">TV Shows</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/people">People</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </div>
+      {toggle && <div className="overlay" onClick={handleToggle} />}
+    </>
   );
 }
 
